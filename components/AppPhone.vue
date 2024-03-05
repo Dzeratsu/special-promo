67d39c1c514cbd31beea74e42e4ea3dc0d2481ba
dzeratsu <a498008@yandex,ru>
2024-03-06 03:34:03 +0400

    buttpn

---
+++ components/AppPhone.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="fixed z-50 right-[70px] bottom-[120px] pointer hidden lg:block">
+    <div class="grid gap-10">
+      <button v-if="openButton" class="p-[20px] rounded-full bg-[#FF802E] test">
+        <img
+          src="img/phone.svg"
+          alt="scroll"
+          width="31"
+          height="31"
+          class="text-xl"
+        />
+      </button>
+      <button v-if="openButton" class="p-[22px] rounded-full bg-[#282421] test">
+        <img
+          src="img/chat.svg"
+          alt="scroll"
+          width="28"
+          height="24"
+          class="text-xl"
+        />
+      </button>
+      <button v-if="!openButton" class="p-[20px] rounded-full bg-[#FF802E] test" @click="openButton = !openButton">
+        <img
+          src="img/phone.svg"
+          alt="scroll"
+          width="31"
+          height="31"
+          class="text-xl"
+        />
+      </button>
+      <button v-else class="p-[20px] rounded-full bg-[#666666] test" @click="openButton = !openButton">
+      <img
+        src="img/cross.svg"
+        alt="scroll"
+        width="31"
+        height="31"
+        class="text-xl"
+      />
+    </button>
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AppPhone',
+  data () {
+    return {
+      openButton: false,
+      text: 'VVVVTEXT'
+    }
+  }
+}
+</script>
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,7 @@
       <AppReviews class="max-w-[1170px] mx-auto" />
     </div>
     <AppScrollToTo />
+    <AppPhone/>
     <AppMarketplace class="max-w-[1170px] mx-auto"/>
     <AppFooter/>
   </main>
