cf88844b8c084a2d494430a6c07d372922ca9cbe
dzeratsu <a498008@yandex,ru>
2024-03-14 07:12:56 +0400

    new fix

--- backend/libs/nodemailer.ts
+++ backend/libs/nodemailer.ts
@@ -1,5 +1,7 @@
 import nodemailer from 'nodemailer'
 import message from '../enum/message'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const config = {
   service: 'gmail',
@@ -11,7 +13,7 @@ const config = {
   port: 587,
   auth: {
     user: 'tehnop18@gmail.com',
-    pass: 'gqaa vvsq apfz oeuy',
+    pass: process.env.MAIL_PASS
   },
 }
 
@@ -19,8 +21,7 @@ const connect = nodemailer.createTransport(config)
 
 connect.verify((err) => {
   if (err) {
-    console.error(err)
-    throw new Error(message.failMail)
+    console.info(err)
   } else {
     console.info(message.startMail)
   }
