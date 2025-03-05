43c33b31d9ee2a5e6784f33948c9b2b19bcc7f80
Andrei Erokhin <80600804+Dzeratsu@users.noreply.github.com>
2025-03-05 09:44:52 +0400

    Update AppMarketplace.vue

--- components/AppMarketplace.vue
+++ components/AppMarketplace.vue
@@ -46,6 +46,21 @@
           Покупайте на маркетплейсах
         </p>
         <div class="flex gap-x-[30px] mt-[19px] justify-center">
+                 <a href="https://www.ozon.ru/search/?from_global=true&text=pirilax+special" target="_blank">
+            <nuxt-img
+              format="webp"
+              alt="Купить Pirilax Special на ozon"
+              quality="100" src="img/ozon-mob.png" width="58" height="58"
+          /></a>
+                 <a
+            href="https://market.yandex.ru/search?suggest_text=%D0%9D%D0%9E%D0%A0%D0%A2&vendorId=13323952&rs=eJxNkD1Iw1AUhd_TommmjDV1uG6COKQVRVMhdBIHFRcXwfjXRUc7uTwoiA7d3FzSVSqI4OIPxkGEjkoHcTA6iJPgIIqTOQcpLofD-S7n3vcK9e45faNVWJmEzvipymsJ_vkc_m4i1aSOJPoADY-YG3h1jVztQuP8RapmFjR4Y34CH5_BJ0OgyT6SsE0_DZV57DUD7K-Bqlv44BRqquNIfthwDJVv7E22r9DcAjUe5xc5c8jmKXRGX-y04KXGLT5oqOHjYV6yx1fs8L2fbLjnW5qg0QHzNn5Ammx74Mwjb3jhPcKZ_ksk5VKnX7L0Df6Dywuf-D-G_QEveeeWDb-lLW901SuujIyVF-y8bVnakVxGMk6Pm11bryxXN7eWCqIGVQfaYv-HRcI-Qp3Top0ut_cPAjn6F-Uem6Y%2C&glfilter=7893318%3A13323952"
+            target="_blank"
+          >
+            <nuxt-img
+              format="webp"
+              alt="Купить Pirilax Special на яндекс маркете"
+              quality="100" src="img/ya-mob.png" width="58" height="58"
+          /></a>
           <a
             href="https://www.wildberries.ru/catalog/0/search.aspx?search=%D0%9F%D0%B8%D1%80%D0%B8%D0%BB%D0%B0%D0%BA%D1%81%20%D0%A1%D0%BF%D0%B5%D1%88%D0%BB"
             target="_blank"
@@ -54,21 +69,6 @@
               format="webp"
               alt="Купить Pirilax Special на wildberess"
               quality="100" src="img/will-mob.png" width="58" height="58"
-          /></a>
-          <a href="https://www.ozon.ru/search/?from_global=true&text=pirilax+special" target="_blank">
-            <nuxt-img
-              format="webp"
-              alt="Купить Pirilax Special на ozon"
-              quality="100" src="img/ozon-mob.png" width="58" height="58"
-          /></a>
-          <a
-            href="https://market.yandex.ru/search?suggest_text=%D0%9D%D0%9E%D0%A0%D0%A2&vendorId=13323952&rs=eJxNkD1Iw1AUhd_TommmjDV1uG6COKQVRVMhdBIHFRcXwfjXRUc7uTwoiA7d3FzSVSqI4OIPxkGEjkoHcTA6iJPgIIqTOQcpLofD-S7n3vcK9e45faNVWJmEzvipymsJ_vkc_m4i1aSOJPoADY-YG3h1jVztQuP8RapmFjR4Y34CH5_BJ0OgyT6SsE0_DZV57DUD7K-Bqlv44BRqquNIfthwDJVv7E22r9DcAjUe5xc5c8jmKXRGX-y04KXGLT5oqOHjYV6yx1fs8L2fbLjnW5qg0QHzNn5Ammx74Mwjb3jhPcKZ_ksk5VKnX7L0Df6Dywuf-D-G_QEveeeWDb-lLW901SuujIyVF-y8bVnakVxGMk6Pm11bryxXN7eWCqIGVQfaYv-HRcI-Qp3Top0ut_cPAjn6F-Uem6Y%2C&glfilter=7893318%3A13323952"
-            target="_blank"
-          >
-            <nuxt-img
-              format="webp"
-              alt="Купить Pirilax Special на яндекс маркете"
-              quality="100" src="img/ya-mob.png" width="58" height="58"
           /></a>
         </div>
       </div>
