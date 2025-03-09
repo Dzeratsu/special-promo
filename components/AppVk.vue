ce8fde237a99230a97f47688f44b1ad4eb13e6ef
Andrei Erokhin <80600804+Dzeratsu@users.noreply.github.com>
2025-03-10 02:47:33 +0400

    Create AppVk.vue

---
+++ components/AppVk.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class=" w-full bg-[#0077FF] min-h-[123px]">
+    <div class="mx-auto max-w-[1170px] py-[33px] px-4 lg:px-0">
+    <div class="flex  flex-col lg:flex-row items-center lg:gap-[90px]">
+      <div class="text-white text-[16px] lg:text-[25px] lg:leading-[30px] font-semibold items-center align-middle">
+        Наша группа ВКонтакте — <br>
+        узнайте больше о производителе профессиональных составов для дерева!
+      </div>
+      <a href="https://vk.com/ooonort" target="_blank" class="block max-sm:w-full">
+      <div class="mt-6 lg:mt-0 bg-white rounded-[10px] w-full lg:w-[230px] h-[42px] align-middle text-center text-black  text-[16px] lg:text-[18px] font-semibold flex items-center justify-center hover:shadow-xl">
+        Перейти
+      </div>
+      </a>
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AppVk'
+}
+</script>
